e8ef0c3bc5b808ea79c5a9ba1e4dbbd464a2ce18
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2022-10-21 09:40:10 +0300

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    def name=(value)
+        @name = value
+    end
+    def breed=(value)
+        @breed = value
+    end
+    def name
+        @name
+    end
+    def breed
+        @breed
+    end
 end
